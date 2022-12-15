1aae74d2566f68235a14e789ee223fadab102b92
YpL68 <yuriy.plks@gmail.com>
2022-12-15 10:00:55 +0200

    Init 02

---
+++ HomeWork_11/app/db_models.py
@@ -0,0 +1,88 @@
+from sqlalchemy import Table, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import mapper, relationship
+from sqlalchemy.sql.sqltypes import DateTime
+from sqlalchemy.sql.functions import now
+from sqlalchemy.ext.hybrid import hybrid_property
+
+from app import db
+
+DATE_FORMAT = "%d.%m.%Y"
+
+notes_tags = Table(
+    "notes_tags",
+    db.metadata,
+    Column("note_id", None, ForeignKey("notes.id", ondelete="CASCADE"), primary_key=True),
+    Column("tag_id", None, ForeignKey("tags.id", ondelete="CASCADE"), primary_key=True),
+)
+
+
+class NotesTags:
+    note_id = None
+    tag_id = None
+
+
+mapper(NotesTags, notes_tags)
+
+
+class BaseModel(db.Model):
+    __abstract__ = True
+
+    id = Column(Integer, nullable=False, primary_key=True, autoincrement=True)
+    created_on = Column(DateTime, nullable=False, server_default=now())
+    updated_on = Column(DateTime, nullable=False, server_default=now(), onupdate=now())
+
+
+class Contact(BaseModel):
+    __tablename__ = "contacts"
+
+    first_name = Column(String(64), index=True, nullable=False)
+    last_name = Column(String(64))
+
+    @hybrid_property
+    def full_name(self):
+        return self.first_name + " " + self.last_name
+
+    email = Column(String(64), unique=True)
+    birthday = Column(DateTime)
+    address = Column(String(128))
+    phone_list = relationship("Phone", cascade="all, delete-orphan", back_populates="contact")
+
+    @hybrid_property
+    def data_view(self) -> dict:
+        return {"id": self.id,
+                "first_name": self.first_name,
+                "last_name": self.last_name,
+                "email": self.email,
+                "birthday": self.birthday,
+                "address": self.address,
+                "phone_list": [phone.phone_num for phone in self.phone_list]}
+
+
+class Phone(BaseModel):
+    __tablename__ = "phones"
+
+    contact_id = Column(None, ForeignKey("contacts.id", ondelete="CASCADE"), nullable=False)
+    phone_num = Column(String(12), nullable=False, index=True, unique=True)
+    contact = relationship("Contact", back_populates="phone_list")
+
+
+class Note(BaseModel):
+    __tablename__ = "notes"
+
+    header = Column(String(64), nullable=False, index=True, unique=True)
+    content = Column(String(512))
+    tags = relationship("Tag", secondary=notes_tags, back_populates="notes")
+
+    @hybrid_property
+    def data_view(self) -> dict:
+        return {"id": self.id,
+                "header": self.header,
+                "content": self.content,
+                "tag_list": [tag.name for tag in self.tags]}
+
+
+class Tag(BaseModel):
+    __tablename__ = 'tags'
+
+    name = Column(String(16), nullable=False, index=True, unique=True)
+    notes = relationship("Note", secondary=notes_tags, back_populates="tags")
---
+++ HomeWork_11/app/repository/notes.py
@@ -0,0 +1,89 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import or_
+
+from app.db_models import Note, NotesTags, Tag
+
+
+def get_note_data_view(session: Session, note_id) -> dict:
+    if note_id == -1:
+        return {"id": -1, "header": "", "content": "", "tag_list": []}
+    else:
+        note = session.query(Note).get(note_id)
+        if not note:
+            raise ValueError(f"Note by id {note_id} not found.")
+        return note.data_view
+
+
+def note_data_view_to_list(data_view: dict) -> list:
+    return [
+        str(data_view["id"]),
+        data_view["header"],
+        data_view["content"] if data_view["content"] else "",
+        ", ".join([tag for tag in data_view["tag_list"]])
+    ]
+
+
+def get_notes(session: Session, filter_str: str = None) -> list:
+    out_list = []
+    f_str = f"%{filter_str}%"
+    if filter_str:
+        notes = session.query(Note).distinct() \
+            .outerjoin(NotesTags).outerjoin(Tag) \
+            .filter(or_(Note.header.like(f_str), Tag.name.like(f_str))) \
+            .order_by(Note.header) \
+            .all()
+    else:
+        notes = session.query(Note).all()
+
+    if notes:
+        out_list.extend([note.data_view for note in notes])
+
+    return out_list
+
+
+def note_tag_list_syn(session: Session, note: Note, tag_list: list):
+    tag_for_delete = []
+    tag_for_append = []
+
+    for tag in note.tags:
+        if tag.name in tag_list:
+            tag_list.remove(tag.name)
+        else:
+            tag_for_delete.append(tag)
+
+    for tag_name in tag_list:
+        tag = session.query(Tag).filter(Tag.name == tag_name).first()
+        if not tag:
+            tag = Tag(name=tag_name)
+        tag_for_append.append(tag)
+
+    [note.tags.remove(tag) for tag in tag_for_delete]
+    note.tags.extend(tag_for_append)
+
+
+def note_insert_or_update(session: Session, data_view: dict):
+    if data_view["id"] != -1:
+        note = session.query(Note).get(data_view["id"])
+        if not note:
+            raise ValueError("No note was found")
+    else:
+        note = Note()
+
+    note.header = data_view["header"]
+    note.content = data_view["content"]
+
+    note_tag_list_syn(session, note, data_view["tag_list"])
+    session.add(note)
+
+    session.commit()
+
+    return "Operation was successful"
+
+
+def note_delete(session: Session, note_id: int):
+    note = session.query(Note).get(note_id)
+    if not note:
+        raise ValueError(f"Note by id {note_id} not found.")
+    session.delete(note)
+    session.commit()
+    return "Operation was successful"
