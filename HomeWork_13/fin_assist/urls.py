e8fcd6d1880596724a3adb403d396a874bd31657
YpL68 <yuriy.plks@gmail.com>
2022-12-25 21:47:58 +0200

    Categories full

--- HomeWork_13/fin_assist/urls.py
+++ HomeWork_13/fin_assist/urls.py
@@ -6,5 +6,6 @@ app_name = 'fin_assist'
 urlpatterns = [
     path('', views.main, name='main'),
     path('categories/', views.pay_categories, name='pay_categories'),
-    path('categories/<int:cat_id>', views.edit_pay_categories, name='edit_pay_categories'),
+    path('categories/edit/<int:cat_id>', views.edit_pay_categories, name='edit_pay_categories'),
+    path('categories/delete/<int:cat_id>', views.delete_pay_categories, name='delete_pay_categories'),
 ]
--- HomeWork_13/fin_assist/views.py
+++ HomeWork_13/fin_assist/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from . models import FinCategory
+from django.contrib import messages
 
 
 def main(request):
@@ -6,10 +8,47 @@ def main(request):
 
 
 def pay_categories(request):
-    return render(request, 'fin_assist/pay_categories.html', context={"filter_str": "ghjfhgfjgh"})
+    if request.method == 'POST':
+        filter_str = request.POST["filter_str"]
+    else:
+        filter_str = ""
+
+    if filter_str:
+        categories = FinCategory.objects.filter(name__icontains=filter_str).order_by("name")
+    else:
+        categories = FinCategory.objects.all().order_by("name")
+
+    return render(request, 'fin_assist/pay_categories.html',
+                  context={"filter_str": filter_str, "categories": categories})
 
 
 def edit_pay_categories(request, cat_id):
     if request.method == 'POST':
-        pass
-    return render(request, 'fin_assist/pay_categories.html', context={"filter_str": str(cat_id)})
+        try:
+            if cat_id == 0:
+                cat = FinCategory(
+                    name=request.POST['cat_name'],
+                    description=request.POST['cat_desc']
+                )
+                cat.save()
+            else:
+                cat = FinCategory.objects.get(pk=cat_id)
+                print(cat.name)
+                cat.name = request.POST['cat_name']
+                cat.description = request.POST['cat_desc']
+                cat.save()
+
+            messages.add_message(request, messages.SUCCESS, "Operation was successful", "success")
+        except Exception as err:
+            messages.add_message(request, messages.ERROR, f"Operation was aborted: {str(err)}", "danger")
+    return redirect('/categories/')
+
+
+def delete_pay_categories(request, cat_id):
+    try:
+        cat = FinCategory.objects.get(pk=cat_id)
+        cat.delete()
+        messages.add_message(request, messages.SUCCESS, "Operation was successful", "success")
+    except Exception as err:
+        messages.add_message(request, messages.ERROR, f"Operation was aborted: {str(err)}", "danger")
+    return redirect('/categories/')
