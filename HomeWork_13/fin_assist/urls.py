e4b3295e7a04e1e1871bf6481316822c211a4616
YpL68 <yuriy.plks@gmail.com>
2022-12-25 14:44:45 +0200

    Add categories 01

--- HomeWork_13/fin_assist/urls.py
+++ HomeWork_13/fin_assist/urls.py
@@ -5,5 +5,6 @@ app_name = 'fin_assist'
 
 urlpatterns = [
     path('', views.main, name='main'),
-    path('categories/', views.pay_categories, name='pay_categories')
+    path('categories/', views.pay_categories, name='pay_categories'),
+    path('categories/<int:cat_id>', views.edit_pay_categories, name='edit_pay_categories'),
 ]
